9caeddf2c00cbe77c47440770e55a5aa78142637
Ved Piyush <piyus001@umn.edu>
2020-02-20 22:42:57 -0600

    added a watershed implementation
    Added a watershed implementation , it does get some beans, some hyper-parameters might have to be tuned.

--- segementation_replicated_and_floodfill.py
+++ segementation_replicated_and_floodfill.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 from scipy import ndimage
 from skimage import measure, color, io
 import numpy as np
+import cmapy
 
 # read in the image, convert to float
 img = cv2.imread(r"D:\Jellybean\data\7UP(R).png")/255.0
@@ -124,8 +125,8 @@ cv2.destroyAllWindows()
 
 # highlight mask on image like R code
 # convert the erosion mask to 3d
-erosion = np.dstack([erosion]*3)
-overlay = cv2.addWeighted(cv2.convertScaleAbs(img*255),0.9,cv2.convertScaleAbs(erosion*255),0.1,0)
+erosion1 = np.dstack([erosion]*3)
+overlay = cv2.addWeighted(cv2.convertScaleAbs(img*255),0.5,cv2.convertScaleAbs(erosion1*255),0.5,0)
 
 # visualize
 cv2.imshow('mask', overlay)
@@ -134,6 +135,85 @@ cv2.destroyAllWindows()
 
 ########### code replication from R complete for one jellybean########
 ########## have to apply to all jellybeans#####################
+
+
+########## watershed attempt #################
+# Finding sure foreground area
+
+# sure background area
+# then dilate
+kern = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(35,35))
+sure_bg = cv2.dilate(erosion,kern,iterations = 5)
+# visualize
+cv2.imshow('mask', sure_bg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# then erode
+kern = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+erosion = cv2.erode(erosion,kern,iterations = 6)
+
+
+
+
+# visualize
+cv2.imshow('mask', erosion)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# then dilate
+kern = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(35,35))
+dilate = cv2.dilate(erosion,kern,iterations = 1)
+
+# visualize
+cv2.imshow('mask', dilate)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+dist_transform = cv2.distanceTransform(cv2.convertScaleAbs(dilate*255),cv2.DIST_L2,5)/255
+# visualize
+cv2.imshow('mask', dist_transform)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+ret, sure_fg = cv2.threshold(dist_transform,0.5*dist_transform.max(),1,0)
+# visualize
+cv2.imshow('mask', sure_fg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(cv2.convertScaleAbs(sure_bg*255),sure_fg)
+# visualize
+cv2.imshow('mask', unknown)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# Marker labelling
+ret, markers = cv2.connectedComponents(sure_fg)
+
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+
+img = cv2.convertScaleAbs(img*255)
+
+
+markers = cv2.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+
+
+# visualize
+cv2.imshow('mask', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 ########## from here on is the code to try a floodfill #####################
